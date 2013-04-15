a94ece90340cb5811b4c51c96916fbc5fe691299
Chris Hunt <c@chrishunt.co>
2013-04-14 20:35:19 -0700

    Extract shared variables, clean up test docs

--- spec/unit/github/auth/keys_file_spec.rb
+++ spec/unit/github/auth/keys_file_spec.rb
@@ -5,11 +5,19 @@ require 'github/auth/keys_file'
 describe Github::Auth::KeysFile do
   subject { described_class.new path: path }
 
-  let(:path) { nil }
+  let(:keys) { %w(abc123 def456) }
+  let(:keys_file) { Tempfile.new 'authorized_keys' }
+  let(:path) { keys_file.path }
+
+  after { keys_file.unlink } # clean up, delete tempfile
 
   describe '#initialize' do
-    it 'has a default path' do
-      expect(subject.path).to_not be_nil
+    context 'without a path' do
+      let(:path) { nil }
+
+      it 'has a default path' do
+        expect(subject.path).to_not be_nil
+      end
     end
 
     context 'with a custom path' do
@@ -22,13 +30,7 @@ describe Github::Auth::KeysFile do
   end
 
   describe '#write!' do
-    let(:keys) { %w(abc123 def456) }
-    let(:path) { keys_file.path }
-    let(:keys_file) { Tempfile.new 'authorized_keys' }
-
-    after { keys_file.unlink } # clean up, delete tempfile
-
-    it 'writes the given keys to the keys file' do
+    it 'writes each key to the keys file' do
       subject.write! keys
       file_content = keys_file.read
       keys.each { |key| expect(file_content).to include key }
@@ -43,7 +45,7 @@ describe Github::Auth::KeysFile do
       end
     end
 
-    context 'with keys already in the keys file' do
+    context 'with existing keys in the keys file' do
       let(:existing_keys) { %w(ghi789 jkl123) }
 
       before do
@@ -51,13 +53,13 @@ describe Github::Auth::KeysFile do
         keys_file.rewind
       end
 
-      it 'preserves the keys' do
+      it 'preserves the existing keys' do
         subject.write! keys
         file_lines = keys_file.readlines
         existing_keys.each { |key| expect(file_lines).to include "#{key}\n" }
       end
 
-      it 'does not put a duplicate key into the keys file' do
+      it 'does not write duplicate keys into the keys file' do
         subject.write! existing_keys.first
         expect(keys_file.readlines.count).to eq existing_keys.count
       end
@@ -65,10 +67,6 @@ describe Github::Auth::KeysFile do
   end
 
   describe '#delete!' do
-    let(:keys) { %w(abc123 def456) }
-    let(:path) { keys_file.path }
-    let(:keys_file) { Tempfile.new 'authorized_keys' }
-
     before do
       keys_file.write keys.join("\n")
       keys_file.rewind
@@ -83,22 +81,22 @@ describe Github::Auth::KeysFile do
         expect(keys_file.read).to_not include key
       end
 
-      it 'does not remove other keys' do
+      it 'does not remove the other key from the keys file' do
         subject.delete! key
         expect(keys_file.read).to include other_key
       end
     end
 
     context 'when the keys file does not have the key' do
-      let(:key) { 'notinthefile' }
+      let(:key) { 'not-in-the-keys-file' }
 
-      it 'does not modify the file' do
-        original_content = keys_file.read
+      it 'does not modify the keys file' do
+        original_keys_file = keys_file.read
         keys_file.rewind
 
         subject.delete! key
 
-        expect(keys_file.read).to eq original_content
+        expect(keys_file.read).to eq original_keys_file
       end
     end
   end
