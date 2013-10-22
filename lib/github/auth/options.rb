599e5d46536369f75737698fe9b5cb82e3218c9f
Chris Hunt <chrishunt@github.com>
2013-10-22 12:16:17 -0700

    Reformat usage in Github::Auth::Options

--- lib/github/auth/options.rb
+++ lib/github/auth/options.rb
@@ -10,7 +10,8 @@ module Github::Auth
         opts.banner = [
           'usage: gh-auth',
           '[--version]',
-          '[add|remove|list]',
+          '[--list]',
+          '[--add|--remove]',
           '<username>'
         ].join(' ')
 
