ef8663a7fdbe55ed75ef2cd5f50c238d04cfeaf1
Chris Hunt <chrishunt@github.com>
2013-10-24 20:21:17 -0700

    Replace OptionsParser with Thor

--- lib/github/auth/cli.rb
+++ lib/github/auth/cli.rb
@@ -1,40 +1,72 @@
+require 'thor'
+
 module Github::Auth
   # Command Line Interface for parsing and executing commands
-  class CLI
-    attr_reader :options
+  class CLI < Thor
+    option :users, type: :array, required: true
+    option :command, type: :string
+    desc 'add', 'Add GitHub users to authorized keys'
+    long_desc <<-LONGDESC
+        `gh-auth add` is used to add one or more GitHub user's public SSH keys
+        to ~/.ssh/authorized_keys. All keys stored on github.com for that
+        user will be added.
 
-    def execute(args)
-      @options = Options.new.parse(args)
-      send options.command
-    end
+        > $ gh-auth add --users=chrishunt zachmargolis
+        \x5> Adding 6 key(s) to '/Users/chris/.ssh/authorized_keys'
 
-    private
+        By default, users will be granted normal shell access. If you'd like to
+        specify an ssh command that should execute when the user connects, use
+        the `--command` option.
 
+        > $ gh-auth add --users=chrishunt --command="tmux attach"
+    LONGDESC
     def add
       on_keys_file :write!,
-        "Adding #{keys.count} key(s) to '#{keys_file.path}'"
+        "Adding #{keys(options[:users]).count} key(s) to '#{keys_file.path}'",
+        { command: options[:command] }
     end
 
+    option :users, type: :array, required: true
+    desc 'remove', 'Remove GitHub users from authorized keys'
+    long_desc <<-LONGDESC
+        `gh-auth remove` is used to remove one or more GitHub user's public SSH
+        keys from ~/.ssh/authorized_keys. All keys stored on github.com for
+        that user will be removed.
+
+        > $ gh-auth remove --users=chrishunt zachmargolis
+        \x5> Removing 6 key(s) to '/Users/chris/.ssh/authorized_keys'
+    LONGDESC
     def remove
       on_keys_file :delete!,
-        "Removing #{keys.count} key(s) from '#{keys_file.path}'"
+        "Removing #{keys(options[:users]).count} key(s) from '#{keys_file.path}'"
     end
 
+    desc 'list', 'List all GitHub users already added to authorized keys'
+    long_desc <<-LONGDESC
+        `gh-auth list` will list all GitHub users that have been added to
+        ~/.ssh/authorized_keys by `gh-auth`.
+
+        > $ gh-auth list
+        \x5> chrishunt, zachmargolis
+    LONGDESC
     def list
-      puts "Added users: #{keys_file.github_users.join(', ')}"
+      puts keys_file.github_users.join(', ')
     end
 
+    desc 'version', 'Show gh-auth version'
     def version
       puts Github::Auth::VERSION
     end
 
-    def usage
-      puts options.usage
+    private
+
+    def keys(usernames = [])
+      @keys ||= Array(usernames).map { |user| keys_for user }.flatten.compact
     end
 
-    def on_keys_file(action, message)
+    def on_keys_file(action, message, options = {})
       puts message
-      rescue_keys_file_errors { keys_file.send action, keys }
+      rescue_keys_file_errors { keys_file(options).send action, keys }
     end
 
     def rescue_keys_file_errors
@@ -51,10 +83,6 @@ module Github::Auth
       puts "  $ touch #{keys_file.path}"
     end
 
-    def keys
-      @keys ||= options.usernames.map { |user| keys_for user }.flatten.compact
-    end
-
     def keys_for(username)
       Github::Auth::KeysClient.new(
         hostname: github_hostname,
@@ -68,12 +96,9 @@ module Github::Auth
       puts "https://status.github.com"
     end
 
-    def keys_file
-      Github::Auth::KeysFile.new keys_file_options
-    end
-
-    def keys_file_options
-      options.keys_file_options.merge path: keys_file_path
+    def keys_file(options = {})
+      Github::Auth::KeysFile.new \
+        options.merge path: keys_file_path
     end
 
     def keys_file_path
